da7232404d3f0b424d6612ecb9aaea310eda369b
Alex Hall <alex.mojaki@gmail.com>
2024-12-07 17:17:45 +0200

    Upgrade to sdk version 35, upgrade gradle, include colorpicker lib manually

--- app/src/test/java/com/alexmojaki/quiggles/MathsTests.kt
+++ app/src/test/java/com/alexmojaki/quiggles/MathsTests.kt
@@ -7,15 +7,15 @@ import org.junit.Assert.*
 class MathsTests {
     @Test
     fun star_isCorrect() {
-        assertEquals(star(119f), Pair(120f, 3))
-        assertEquals(star(120f), Pair(120f, 3))
-        assertEquals(star(121f), Pair(120f, 3))
+        assertEquals(star(119.0), Pair(120.0, 3))
+        assertEquals(star(120.0), Pair(120.0, 3))
+        assertEquals(star(121.0), Pair(120.0, 3))
 
-        assertEquals(star(10f), Pair(40f, 9))
-        assertEquals(star(350f), Pair(320f, 9))
+        assertEquals(star(10.0), Pair(40.0, 9))
+        assertEquals(star(350.0), Pair(320.0, 9))
 
-        assertEquals(star(90f), Pair(90f, 4))
-        assertEquals(star(95f), Pair(90f, 4))
+        assertEquals(star(90.0), Pair(90.0, 4))
+        assertEquals(star(95.0), Pair(90.0, 4))
     }
 
 
